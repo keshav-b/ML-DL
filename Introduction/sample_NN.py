f47e0164c268dc805c920db998a64bdacf7226b4
Keshav Balachandar <30681515+keshav-b@users.noreply.github.com>
2020-03-26 17:58:53 +0530

    sample Neural Network

--- Introduction/sample_NN.py
+++ Introduction/sample_NN.py
@@ -3,32 +3,38 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Network(nn.Module):
-	def __init__(self):
-		super(Network, self).__init__()
-
-		self.hidden = nn.Linear(2,2)
-		self.output = nn.Linear(1,1)
+	def __init__(self, inputs=4, hidden1=8, hidden2=9, outputs=3):
+		super().__init__()
+		self.fc1 = nn.Linear(inputs, hidden1)
+		self.fc2 = nn.Linear(hidden1, hidden2)
+		self.out = nn.Linear(hidden2, outputs)
 
 	def forward(self, x):
-		x = self.hidden(x)
-		x = torch.tanh(x)
-		x = self.output(x)
-		x = torch.softmax(x)
+		x = self.fc1(x)
+		x = F.relu(x)
+		x = self.fc2(x)
+		x = F.relu(x)
+		x = self.out(x)
 		return x
 
 model = Network()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
+# define x_train, y_train, x_test, y_test 
+losses = []
+
 for i in range(epochs):
-	y_pred = model(x)
-	loss = criterion(y_pred, y)
+	y_pred = model(x_train)  # or model.forward(x_train)
+
+	loss = criterion(y_pred, y_train)
+	losses.append(loss)
 	'''
-	loss.item() gives the loss
-	total_loss +=loss.item()
+	if(i%10 == 0):
+		print(f'Epoch: {i} | Loss: {loss}')
 	'''
 	optimizer.zero_grad()
 	loss.backward()
 	optimizer.step()
 
-	# loss_history.append(total_loss/no_of_samples)	
+
