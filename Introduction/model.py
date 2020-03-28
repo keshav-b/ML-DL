7351f79607f6720d072d86cc1c870ea4bd060b4d
Keshav Balachandar <30681515+keshav-b@users.noreply.github.com>
2020-03-28 21:52:27 +0530

    updated: testing and saving

--- Introduction/model.py
+++ Introduction/model.py
@@ -1,7 +1,9 @@
+# IMPORT LIBRARIES
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# MODULE CREATION
 class Network(nn.Module):
 	def __init__(self, inputs=3, hidden1=2, outputs=1):
 		super().__init__()
@@ -15,24 +17,60 @@ class Network(nn.Module):
 		x = self.softmax(x, dim=0)
 		return x
 
+# INSTANTIATE PARAMETERS
 model = Network()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
+# TRAINING
 # define x_train, y_train, x_test, y_test 
 losses = []
-
 for i in range(epochs):
 	y_pred = model(x_train)  # or model.forward(x_train)
 
 	loss = criterion(y_pred, y_train)
 	losses.append(loss)
-	'''
-	if(i%10 == 0):
+	
+	if(i%5 == 0):
 		print(f'Epoch: {i} | Loss: {loss}')
-	'''
+	
 	optimizer.zero_grad()
 	loss.backward()
 	optimizer.step()
+
+# VALIDATION	
+with torch.no_grad(): # deactivate the auto gradient engine, no back prop
+	y_eval = model.forward(x_test)
+	loss = criterion(y_eval, y_test)
+# print(loss)
+
+# VIEWING THE RESULTS OF THE TEST DATA
+with torch.no_grad():
+
+	print("Model Predicted \t\t\t Actual Output")
+	for data in x_test:
+		y_val = model.forward(data)
+
+		print(f"{str(y_val)} {y_test[i]}")
+
+
+# SAVING THE MODEL
+torch.save(model.state_dict(), "sample_model.pt")		
+
+# USING THE SAVED MODEL
+my_model = Network()
+my_model.load_state_dict(torch.load("sample_model.pt"))
+
+# TESTING WITH NEWLY LOADED MODEL
+new_test = torch.tensor('''[new test data]''' )
+with torch.no_grad():
+	print(my_model(new_test))
+
+
+
+		
+
+	
+
 
 
